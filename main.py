731878b581f8f9d1e71a41c5f37fb9f936125c32
AceAttorneyMaster111 <NoahS2003@gmail.com>
2017-04-11 13:02:03 -0400

    Version 0.0.1!

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 #  License, v. 2.0. If a copy of the MPL was not distributed with this
 #  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+# Version: 0.0.1
+
 def i(question):
 	return input(question + " ")
 
